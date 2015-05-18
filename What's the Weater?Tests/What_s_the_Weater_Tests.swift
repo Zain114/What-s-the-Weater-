cd90ce8127848e9b7651ae83b3e93bb3e6c84f51
Zainuddin Qazi <zainuddinqazi@Zainuddins-MacBook-Pro.local>
2015-05-17 23:32:39 -0700

    Initial Commit

---
+++ What's the Weater?Tests/What_s_the_Weater_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  What_s_the_Weater_Tests.swift
+//  What's the Weater?Tests
+//
+//  Created by Zainuddin Qazi on 5/17/15.
+//  Copyright (c) 2015 Zainuddin Qazi. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class What_s_the_Weater_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
